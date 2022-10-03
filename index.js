7fd3896666605f1cc18ab82d4e8364e3f5eb67c8
“ok-matsu” <k-matsuo@esm.co.jp>
2022-10-04 08:14:55 +0900

    web app1-1 1-2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(moment())
+<!DOCTYPE html>
+<html>
+  <head>
+    <base target="_top">
+    <script>
+        let counter,n;
+        function AddCounter(){
+ //         var counter = document.getElementById("disp_count");
+          n ++;
+          disp_count.innerHTML = n;
+        //  alert(n);
+        }
+        function ResetCounter(){
+            n = 0;
+            disp_count.innerHTML = n;
+        }
+      </script>
+  </head>
+  <body>
+    <p id="disp_count">1</p> 
+    <input type = "button" value = "＋１する" id="btn_count_up" onclick="AddCounter();"> 
+    <input type = "button" value = "リセット" id="btn_reset" onclick="ResetCounter();"> 
+    <script>
+        n=1;
+    </script>
+</body>
+</html>
