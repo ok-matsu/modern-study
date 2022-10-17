ffab81098f4473b7d882fbe4b081c955e087676c
“ok-matsu” <k-matsuo@esm.co.jp>
2022-10-17 09:18:50 +0900

    web-app restart

--- web-app1/コード.js
+++
@@ -1,9 +0,0 @@
-function doGet(e){
-  var template = HtmlService.createTemplateFromFile('index');
-  return template.evaluate();
-//  counter = document.getElementById("disp_count");
-//  btnAdd = document.getElementById("btn_count_up");
-//  n=0;
-//  btnAdd.addEventListerner("click",addCount);
-}
-
