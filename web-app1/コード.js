b9d27930c6ce64d620fc02ff9b2e1707117b415f
“ok-matsu” <k-matsuo@esm.co.jp>
2022-11-19 10:15:45 +0900

    webapp 3-1 3-2 指摘とりこみ

--- web-app1/コード.js
+++ web-app1/コード.js
@@ -1,8 +1,4 @@
 function doGet(e){
-  var template = HtmlService.createTemplateFromFile('index');
-  return template.evaluate();
-//  counter = document.getElementById("disp_count");
-//  btnAdd = document.getElementById("btn_count_up");
-//  n=0;
-//  btnAdd.addEventListerner("click",addCount);
+  var template = HtmlService.createTemplateFromFile('index'); 
+  return template.evaluate(); 
 }
